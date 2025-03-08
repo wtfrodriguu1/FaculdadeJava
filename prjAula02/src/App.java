90aae5dc89069e9763a18325114eaf01beb77278
Rodrigo Cardozo <wtfrodriguu@gmail.com>
2025-03-07 21:44:53 -0300

    operacoes

--- prjAula02/src/App.java
+++ prjAula02/src/App.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader:
+import java.io.InputStreamReader;
 
 public class App {
     public static void main(String[] args) throws Exception 
@@ -9,7 +9,7 @@ public class App {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         try {
-            System.out.print("Digite seu nome Completo: ");
+            /*  System.out.print("Digite seu nome Completo: ");
             String nome = reader.readLine();
 
             System.out.print("Digite sua idade: ");
@@ -24,7 +24,29 @@ public class App {
 
         } catch (IOException e)
         {
+            e.printStackTrace();*/
+
+
+            System.out.print("Digite o valor 1: ");
+            int Valor1 = Integer.parseInt(reader.readLine());
+
+            System.out.print("Digite o valor 2: ");
+            int Valor2 = Integer.parseInt(reader.readLine());
+        
+            int soma = Valor1 + Valor2;
+            int subtracao = Valor1 - Valor2;
+            int multiplicacao = Valor1 * Valor2;
+
+            System.out.println("Soma = " + soma);
+            System.out.println("Subtração = " + subtracao);
+            System.out.println("Multiplicação = " + multiplicacao);
+
+        } catch (IOException e)
+        {
             e.printStackTrace();
+
+            //realziar a leitura de duas variáveis (Valor1 e Valor2)
+            //realziar a soma, subtração e multiplicacao e imprima o valor dos resultados
         }
     }
 }
